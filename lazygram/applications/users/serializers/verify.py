2b6f3e86ca14172d840cabe80c956a3b8dde8bb4
NahuelA <nahuelarrascaeta22@gmail.com>
2022-11-18 15:50:03 -0300

    Added verify refresh and username token to activate account

--- lazygram/applications/users/serializers/verify.py
+++ lazygram/applications/users/serializers/verify.py
@@ -1,7 +1,6 @@
 """ Verify token. """
 
 from rest_framework import serializers
-from django.core.cache import cache
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.settings import api_settings
 
