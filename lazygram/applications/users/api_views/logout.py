f5648d34dbef7a40143bc9c045ba9b48cc0e072c
NahuelA <nahuelarrascaeta22@gmail.com>
2022-12-24 22:21:04 -0300

    Remove csrf and session cookies, but not accesstoken cookie

--- lazygram/applications/users/api_views/logout.py
+++ lazygram/applications/users/api_views/logout.py
@@ -12,10 +12,6 @@ class LogoutView(APIView):
 
     def post(self, request):
         response = Response(status=status.HTTP_204_NO_CONTENT)
-
-        if request.COOKIES.get("access_token") != None:
-            response.delete_cookie("access_token", "/")
-            response.delete_cookie("csrftoken", "/")
-            response.delete_cookie("sessionid", "/")
-
+        response.delete_cookie("csrftoken", "/")
+        response.delete_cookie("sessionid", "/")
         return response
