5e668fa0e50c09c11bf005f6a64639242e0c191c
NahuelA <nahuelarrascaeta22@gmail.com>
2023-02-23 23:46:24 -0300

    Modify wsgi.py config file

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 WSGI config for Lazygram project.
 
@@ -13,25 +15,45 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
+# import os
+# import sys
+
+# from django.core.wsgi import get_wsgi_application
+
+# # This allows easy placement of apps within the interior
+# # lazygram directory.
+# app_path = os.path.abspath(
+#     os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+# )
+# sys.path.append(os.path.join(app_path, "lazygram"))
+
+# print(os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production"))
+
+# # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
+# # if running multiple sites in the same mod_wsgi process. To fix this, use
+# # mod_wsgi daemon mode with each site in its own daemon process, or use
+# # os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+
+# # This application object is used by any WSGI server configured to use this
+# # file. This includes Django's development server, if the WSGI_APPLICATION
+# # setting points here.
+# application = get_wsgi_application()
+
+
+"""
+WSGI config for lazygram project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
-# This allows easy placement of apps within the interior
-# lazygram directory.
-app_path = os.path.abspath(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
-)
-sys.path.append(os.path.join(app_path, "lazygram"))
-
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 application = get_wsgi_application()
